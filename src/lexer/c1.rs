05cc31f139954d631d12d45c0cc06dd052d6b9af
wwting7 <105224105+wwting7@users.noreply.github.com>
2022-05-22 19:42:47 +0200

    Update c1.rs

--- src/lexer/c1.rs
+++ src/lexer/c1.rs
@@ -6,7 +6,116 @@ pub enum C1Token {
 
     // Logos requires one token variant to handle errors,
     // it can be named anything you wish.
-    
+    #[token("bool")]
+    KwBoolean,
+
+    #[token("do")]
+    KwDo,
+
+    #[token("else")]
+    KwElse,
+
+    #[token("float")]
+    KwFloat,
+
+    #[token("for")]
+    KwFor,
+
+    #[token("if")]
+    KwIf,
+
+    #[token("int")]
+    KwInt,
+
+    #[token("printf")]
+    KwPrintf,
+
+    #[token("return")]
+    KwReturn,
+
+    #[token("void")]
+    KwVoid,
+
+    #[token("while")]
+    KwWhile,
+
+    #[token("+")]
+    Plus,
+
+    #[token("-")]
+    Minus,
+
+    #[token("*")]
+    Asterisk,
+
+    #[token("/")]
+    Slash,
+
+    #[token("=")]
+    Assign,
+
+    #[token("==")]
+    Eq,
+
+    #[token("!=")]
+    Neq,
+
+    #[token("<")]
+    Lss,
+
+    #[token(">")]
+    Grt,
+
+    #[token("<=")]
+    Leq,
+
+    #[token(">=")]
+    Geq,
+
+    #[token("&&")]
+    And,
+
+    #[token("||")]
+    Or,
+
+    #[token(",")]
+    Comma,
+
+    #[token(";")]
+    Semicolon,
+
+    #[token("(")]
+    LParen,
+
+    #[token(")")]
+    RParen,
+
+    #[token("{")]
+    LBrace,
+
+    #[token("}")]
+    RBrace,
+
+    #[regex("[0-9]+")]
+    ConstInt,
+
+    #[regex("([0-9]"."[0-9]|"."[0-9])([eE]([-+])?[0-9])?|[0-9][eE]([-+])?[0-9]")]
+    ConstFloat,
+
+    #[regex("true|false")]
+    ConstBoolean,
+
+    #[regex("\"[^\n\"]*"\"")]
+    ConstString,
+
+    #[regex("[a-zA-Z]+([a-zA-Z]|[0-9])*")]
+    Id,
+
+    #[regex(r"/**/", logos::skip)]
+    Ignored,
+
+    #[regex(r"//\n", logos::skip)]
+    Ignored,
     
     #[error]
     Error,
